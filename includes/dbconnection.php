0a82246bfe8e69181c5ccc3e1cfdc8a3ad7a2e3c
karadiakhil <karadiakhil@gmail.com>
2021-04-21 01:40:37 +0100

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
-define('DB_HOST','cloud.c876e3yhfbay.eu-west-1.rds.amazonaws.com');
+define('DB_HOST','covid-pass.chdy66fkz2zq.us-east-1.rds.amazonaws.com');
 define('DB_USER','admin');
-define('DB_PASS','Welcome!1234');
+define('DB_PASS','admin123');
 define('DB_NAME','cpms');
 // Establish database connection.
 try
